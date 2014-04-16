678728329dd0d8094067d95f49ff0c884b7575de
Bernhard Mallinger <b.mallinger@gmx.at>
2014-04-16 14:04:22 +0000

    expansion tree tests work
    git-svn-id: http://gapt.googlecode.com/svn/branches/new_lambda_calculus@1995 85d21ef8-add0-11de-ae77-5bfb9c5a3f34

--- source/syntax/calculi/expansion_trees/src/test/scala/at/logic/calculi/expansionTrees/ExpansionTreeTest.scala
+++ source/syntax/calculi/expansion_trees/src/test/scala/at/logic/calculi/expansionTrees/ExpansionTreeTest.scala
@@ -5,7 +5,7 @@ import org.specs2.mutable._
 import org.junit.runner.RunWith
 import org.specs2.runner.JUnitRunner
 import at.logic.language.hol.{Atom => AtomHOL, And => AndHOL, Or => OrHOL, Imp => ImpHOL, _}
-import at.logic.language.lambda.types.{Ti => i}
+import at.logic.language.lambda.types.{Ti => i, To => o, ->}
 
 
 @RunWith(classOf[JUnitRunner])
@@ -15,13 +15,15 @@ class ExpansionTreeTest extends SpecificationWithJUnit {
   val beta = HOLVar(("\\beta" ), i)
   val c = HOLConst(("c" ), i)
   val d = HOLConst(("d" ), i)
-  val f = HOLConst("f", i)
+  val a = HOLConst(("a" ), i)
+  val f = HOLConst("f", i -> i)
   val x = HOLVar(("x" ), i)
   val y = HOLVar(("y" ), i)
   val z = HOLVar(("z" ), i)
-  val eq = HOLConst("=", i)
-  val P = HOLConst("P", i)
-  val Q = HOLConst("Q", i)
+  val eq = HOLConst("=", i -> (i -> o) )
+  val P = HOLConst("P", i -> (i -> (i -> o)))
+  val Q = HOLConst("Q", i -> o)
+  val R = HOLConst("R", i -> o)
 
   val et1 = WeakQuantifier(
     ExVar(x, AtomHOL(eq, x::x::Nil)),
@@ -40,10 +42,10 @@ class ExpansionTreeTest extends SpecificationWithJUnit {
   )
 
   val et4 = WeakQuantifier(
-    ExVar(x, AtomHOL(P, x::c::Nil)),
+    ExVar(x, AtomHOL(P, x::c::a::Nil)),
     List(
-      (Atom( AtomHOL(P, z::c::Nil) ), z),
-      (Atom( AtomHOL(P, y::c::Nil) ), y)
+      (Atom( AtomHOL(P, z::c::a::Nil) ), z),
+      (Atom( AtomHOL(P, y::c::a::Nil) ), y)
     )
   )
 
@@ -92,9 +94,9 @@ class ExpansionTreeTest extends SpecificationWithJUnit {
       val etPrime = substitute.applyNoMerge(s, et4)
 
         etPrime mustEqual WeakQuantifier(
-        ExVar(x, AtomHOL(P, x::c::Nil)),
+        ExVar(x, AtomHOL(P, x::c::a::Nil)),
         List(
-          (MergeNode(Atom(AtomHOL(P, y::c::Nil)), Atom(AtomHOL(P, y::c::Nil))), y)
+          (MergeNode(Atom(AtomHOL(P, y::c::a::Nil)), Atom(AtomHOL(P, y::c::a::Nil))), y)
         )
       )
     }
@@ -106,34 +108,34 @@ class ExpansionTreeTest extends SpecificationWithJUnit {
     }
 
     "do simple substitution as result of merge" in {
-      val etSubst1 = Neg( StrongQuantifier(AllVar(x, AtomHOL(P, x::Nil) ), z, Atom( AtomHOL(P, z::Nil)) ) )
-      val etSubst2 = Neg( StrongQuantifier(AllVar(x, AtomHOL(P, x::Nil) ), y, Atom( AtomHOL(P, y::Nil)) ) )
+      val etSubst1 = Neg( StrongQuantifier(AllVar(x, AtomHOL(Q, x::Nil) ), z, Atom( AtomHOL(Q, z::Nil)) ) )
+      val etSubst2 = Neg( StrongQuantifier(AllVar(x, AtomHOL(Q, x::Nil) ), y, Atom( AtomHOL(Q, y::Nil)) ) )
 
       // from a theoretical point of view, the result could also be equal to the second tree, but users probably expect the algo to work from left to right
       merge( MergeNode(etSubst1, etSubst2) ) mustEqual etSubst1
     }
 
     "do simple substitution as result of merge with context" in {
-      val etSubst1 = Neg( StrongQuantifier(AllVar(x, AtomHOL(P, x::Nil) ), z, Atom( AtomHOL(P, z::Nil)) ) )
-      val etSubst2 = Neg( StrongQuantifier(AllVar(x, AtomHOL(P, x::Nil) ), y, Atom( AtomHOL(P, y::Nil)) ) )
+      val etSubst1 = Neg( StrongQuantifier(AllVar(x, AtomHOL(Q, x::Nil) ), z, Atom( AtomHOL(Q, z::Nil)) ) )
+      val etSubst2 = Neg( StrongQuantifier(AllVar(x, AtomHOL(Q, x::Nil) ), y, Atom( AtomHOL(Q, y::Nil)) ) )
       merge( MergeNode(etSubst1, etSubst2) ) mustEqual etSubst1
     }
 
     "do merge with substitution in other tree of sequent triggered by merge" in {
       // example from Chaudhuri et.al : A multi-focused proof system isomorphic to expansion proofs
-      val etSubst1 = StrongQuantifier(AllVar(x, AtomHOL(P, x::Nil) ), z, Atom( AtomHOL(P, z::Nil)) )
-      val etSubst2 = StrongQuantifier(AllVar(x, AtomHOL(P, x::Nil) ), y, Atom( AtomHOL(P, y::Nil)) )
+      val etSubst1 = StrongQuantifier(AllVar(x, AtomHOL(R, x::Nil) ), z, Atom( AtomHOL(R, z::Nil)) )
+      val etSubst2 = StrongQuantifier(AllVar(x, AtomHOL(R, x::Nil) ), y, Atom( AtomHOL(R, y::Nil)) )
       val fy = Function(f, y::Nil)
       val fz = Function(f, z::Nil)
       val seq = (Nil,
         // only succedent:
         MergeNode(etSubst1, etSubst2) ::
-        WeakQuantifier(ExVar(x, AtomHOL(Q, x::Nil)), List(
-          (Atom(AtomHOL(Q, fz::Nil)), fz ),
-          (Atom(AtomHOL(Q, fy::Nil)), fy )
+        WeakQuantifier(ExVar(x, AtomHOL(R, x::Nil)), List(
+          (Atom(AtomHOL(R, fz::Nil)), fz ),
+          (Atom(AtomHOL(R, fy::Nil)), fy )
         )) ::
-        Atom( AtomHOL(P, z::Nil) ) ::
-        Atom( AtomHOL(P, y::Nil) ) ::
+        Atom( AtomHOL(R, z::Nil) ) ::
+        Atom( AtomHOL(R, y::Nil) ) ::
         Nil
         )
       val mergedSeq = merge(seq)
@@ -141,12 +143,13 @@ class ExpansionTreeTest extends SpecificationWithJUnit {
       // merge will trigger a substitution y -> z
 
       val testResult = new ExpansionSequent(Nil,
-        (StrongQuantifier(AllVar(x, AtomHOL(P, x::Nil)), z, Atom( AtomHOL(P, z::Nil))) ::
-        WeakQuantifier(ExVar(x, AtomHOL(Q, x::Nil)), List( (Atom(AtomHOL(Q, fz::Nil)), fz))) ::
-        Atom( AtomHOL(P, z::Nil) ) ::
-        Atom( AtomHOL(P, z::Nil) ) ::
+        (StrongQuantifier(AllVar(x, AtomHOL(R, x::Nil)), z, Atom( AtomHOL(R, z::Nil))) ::
+        WeakQuantifier(ExVar(x, AtomHOL(R, x::Nil)), List( (Atom(AtomHOL(R, fz::Nil)), fz))) ::
+        Atom( AtomHOL(R, z::Nil) ) ::
+        Atom( AtomHOL(R, z::Nil) ) ::
         Nil).asInstanceOf[Seq[ExpansionTree]]
       )
+
 
       mergedSeq mustEqual testResult
     }
